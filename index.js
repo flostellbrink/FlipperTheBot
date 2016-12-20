8fb245ed8593d0f31cf002bfabe01bec2eb7915b
Owlinated <florian.stellbrink@student.uni-siegen.de>
2016-12-20 20:29:40 +0100

    Add toin cossing and help command

--- index.js
+++ index.js
@@ -5,23 +5,40 @@ const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
 const tg = new Telegram.Telegram('319892880:AAF6oY-0KsysbDAbaZbpm_nUf2kUME3zHqU')
 
-class PingController extends TelegramBaseController {
-    /**
-     * @param {Scope} $
-     */
-    pingHandler($) {
-        $.sendMessage('pong')
+class StartController extends TelegramBaseController {
+    startHandler($) {
+        $.sendMessage('Welcome to owl nation! Have a look at the available commands:')
+        printCommands($)
+    }
+
+    function printCommands($) {
+        $.sendMessage('/toss - Tosses a coin')
     }
 
     get routes() {
         return {
-            'pingCommand': 'pingHandler'
+            'startCommand': 'startHandler'
+            'unknownCommand': 'unknownHandler'
+        }
+    }
+}
+
+class RandomController extends TelegramBaseController {
+    function coinFlip() {
+        return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails'
+    }
+
+    tossHandler($) {
+        $.sendMessage('Tossing a coin. Its ' + coinFlip())
+    }
+
+    get routes() {
+        return {
+            'tossCommand': 'tossHandler'
         }
     }
 }
 
 tg.router
-    .when(
-        new TextCommand('ping', 'pingCommand'),
-        new PingController()
-    )
+    .when(new TextCommand('/start', 'startCommand'), new StartController())
+    .when(new TextCommand('/toss', 'tossCommand'), new RandomController())
