c38b77653eab79fc16cac4818c2a9318d02653f5
Owlinated <florian.stellbrink@student.uni-siegen.de>
2016-12-21 03:09:02 +0100

    Bind express to port supplied by heroku
    Hopefully this helps with the bot being killed

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 'use strict'
 
+var express = require('express');
+var app     = express();
+app.set('port', (process.env.PORT || 5000));
+
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
 const tg = new Telegram.Telegram('319892880:AAF6oY-0KsysbDAbaZbpm_nUf2kUME3zHqU')
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 function printCommands($) {
     $.sendMessage('/toss - Tosses a coin')
